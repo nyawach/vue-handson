4ded50b12ebac0bddb6d98fc723cea75b897a471
himeno-yusuke <himeno-yusuke@kayac.com>
2018-12-20 13:48:40 +0900

    とりあえずTODOリスト作成まで

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form @submit.prevent="addTodo">
+      <input type="text" name="new" id="" v-model="newTodoText">
+      <button :disabled="!newTodoText">new</button>
+    </form>
+    <div class="todo-list">
+      <template v-for="(todo, i) in todos">
+        <Todo :todo="todo" :key="i"></Todo>
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo.vue'
+import todos from './assets/todos.json'
 
 export default {
   name: 'app',
+  data: () =>  ({
+    todos,
+    newTodoText: "",
+  }),
   components: {
-    HelloWorld
+    Todo,
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        text: this.newTodoText,
+        done: false,
+      })
+      this.newTodoText = ""
+    }
   }
 }
 </script>
